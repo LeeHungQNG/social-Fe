cfe4d4652f07e6848ef2fffec1ae341fa576180c
LeeHungQNG <builequochung3004@gmail.com>
2025-08-05 08:04:35 +0700

    fix style in people

--- src/app/(home)/people/page.tsx
+++ src/app/(home)/people/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 const PeoplePage = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <People />;
+      <People />
     </Suspense>
   );
 };
